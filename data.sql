a422693ca2764f469cdc06e023650648afd8cf94
andres-condezo <ra.condezomonge@gmail.com>
2022-03-22 17:08:26 -0500

    Add insert commands

--- data.sql
+++ data.sql
@@ -1,5 +1,9 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals
+(name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES
+('Agumon', '2020-02-03', 0, TRUE, 10.23),
+('Gabumon', '2018-11-15', 2, TRUE, 8.0),
+('Pikachu', '2021-01-07', 1, FALSE, 15.04),
+('Devimon', '2017-05-12', 5, TRUE, 11.0);
