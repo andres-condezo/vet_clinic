d962d9cabf1ecd68f20f2780fb1a58038f3496bc
andres-condezo <ra.condezomonge@gmail.com>
2022-03-22 17:08:38 -0500

    Add queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,57 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+FROM animals
+WHERE name LIKE '%mon';
+
+SELECT name
+FROM animals
+WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+
+SELECT name
+FROM animals
+WHERE neutered = TRUE AND escape_attempts < 3;
+
+SELECT date_of_birth
+FROM animals
+WHERE name IN ('Agumon', 'Pikachu');
+
+SELECT name
+FROM animals
+WHERE weight_kg > 10.5;
+
+SELECT
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+FROM animals
+WHERE neutered = TRUE;
+
+SELECT
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+FROM animals
+WHERE name != 'Gabumon';
+
+SELECT
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+FROM animals
+WHERE weight_kg BETWEEN 10.4 AND 17.3;
