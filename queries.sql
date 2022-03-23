0b2386f0a8434c344548d8d15388171af4dbadc3
andres-condezo <ra.condezomonge@gmail.com>
2022-03-23 05:04:39 -0500

    Add new queries

--- queries.sql
+++ queries.sql
@@ -55,3 +55,107 @@ SELECT
     weight_kg
 FROM animals
 WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+
+SELECT
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg,
+    species
+FROM animals;
+
+ROLLBACK;
+
+SELECT
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg,
+    species
+FROM animals;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+COMMIT;
+
+SELECT
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg,
+    species
+FROM animals;
+
+BEGIN;
+DELETE
+FROM animals;
+
+ROLLBACK;
+
+SELECT
+    id,
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg,
+    species
+FROM animals;
+
+BEGIN;
+DELETE
+FROM animals
+WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT(*)
+FROM animals;
+
+SELECT COUNT(*)
+FROM animals
+WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg)
+FROM animals;
+
+SELECT
+    neutered,
+    MAX(escape_attempts) AS escapes_attempts
+FROM animals
+GROUP BY neutered;
+
+SELECT
+    species,
+    MIN(weight_kg) AS min_weight_kg,
+    MAX(weight_kg) AS max_weight_kg
+FROM animals
+GROUP BY species;
+
+SELECT
+    species,
+    AVG(escape_attempts) AS escapes_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
