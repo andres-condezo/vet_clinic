d35c8eb3c2503f03a87cb3666031e4a710c397e1
andres-condezo <ra.condezomonge@gmail.com>
2022-03-26 03:49:47 -0500

    Create column species_id and owner_id in the animals table

--- schema.sql
+++ schema.sql
@@ -12,3 +12,35 @@ CREATE TABLE animals(
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(250);
+
+CREATE TABLE owners(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(250),
+  age INT,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(250),
+  PRIMARY KEY(id)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT constraint_species
+FOREIGN KEY (species_id)
+REFERENCES species (id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT constraint_owner
+FOREIGN KEY (owner_id)
+REFERENCES owners;
