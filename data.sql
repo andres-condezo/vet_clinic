27dc48973dbafc7aa5909e0014d52815bc45e65b
andres-condezo <ra.condezomonge@gmail.com>
2022-03-26 03:49:16 -0500

    Add new data in species and wners

--- data.sql
+++ data.sql
@@ -18,3 +18,47 @@ VALUES
 ('Boarmon', '2005-06-07', 7, TRUE, 20.4),
 ('Blossom', '1998-10-13', 3, TRUE, 17),
 ('Ditto', '2022-05-14', 4, TRUE, 22);
+
+INSERT INTO owners
+(full_name, age)
+VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species
+(name)
+VALUES
+('Pokemon'),
+('Digimon');
+
+UPDATE animals
+SET species_id = 2
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species_id = 1
+WHERE species_id IS NULL;
+
+UPDATE animals
+SET owner_id = 1
+WHERE name LIKE 'Agumon';
+
+UPDATE animals
+SET owner_id = 2
+WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals
+SET owner_id = 3
+WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals
+SET owner_id = 4
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+SET owner_id = 5
+WHERE name IN ('Angemon', 'Boarmon');
