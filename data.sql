b20f2747bf9705a3d4209c96904f5045fed8c3c5
andres-condezo <ra.condezomonge@gmail.com>
2022-03-28 14:59:50 -0500

    Add example data to my database

--- data.sql
+++ data.sql
@@ -102,3 +102,19 @@ VALUES
 ( 9, 2, '2020-08-03'),
 ( 10, 3, '2020-05-24'),
 ( 10, 1, '2021-01-11');
+
+INSERT
+INTO visits
+(animals_id, vet_id, date_of_visit)
+SELECT *
+FROM
+    (SELECT id FROM animals) animals_ids,
+    (SELECT id FROM vets) vets_ids,
+    generate_series('1980-01-01'::timestamp,'2021-01-01', '4 hours') visit_timestamp;
+
+INSERT
+INTO owners
+(full_name, email)
+SELECT
+    'Owner ' || generate_series(1,2500000),
+    'owner_' || generate_series(1,2500000) || '@mail.com';
