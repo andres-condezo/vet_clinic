e9094c33c509d55deb5a625e41a0906940fc5b97
Fabricio Garcia <fabgrel10@gmail.com>
2022-03-28 15:16:48 -0600

    Add index column to vet_id from visits table

--- schema.sql
+++ schema.sql
@@ -72,5 +72,7 @@ CREATE TABLE visits(
 );
 
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
 CREATE INDEX animals_visits_idx ON visits(animal_id);
 
+CREATE INDEX vets_visits_idx ON visits(vet_id);
