2cfd8ba8ec1151488ba3ec6ca23c7867f4c52eb1
Leonardo <albornozleo95@gmail.com>
2022-03-29 16:45:25 -0400

    Create medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,3 +6,11 @@ CREATE TABLE patients(
   date_of_birth DATE NOT NULL
 );
 
+CREATE TABLE medical_histories(
+  id SERIAL PRIMARY KEY NOT NULL,
+  admitted_at TIMESTAMP NOT NULL,
+  patient_id INTEGER NOT NULL,
+  status VARCHAR(150) NOT NULL,
+  CONSTRAINT constraint_patient FOREIGN KEY(patient_id) REFERENCES patients(id)
+);
+
